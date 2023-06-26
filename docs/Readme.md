a5ba2747da8c69236a97347a0073c15f544603ea
tacks9 <tacks321@qq.com>
2023-06-26 15:31:58 +0800

    📃 docs(sql): 数据库建表语句

---
+++ docs/Readme.md
@@ -0,0 +1,7 @@
+# 项目说明
+
+
+## 数据库
+
+- [创建数据库](./sql/create_db.sql)
+- [创建数据表](./sql/create_table.sql)
---
+++ docs/sql/create_db.sql
@@ -0,0 +1,3 @@
+-- blog 数据库结构
+create database blog CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `blog`;
