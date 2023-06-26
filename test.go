eae81949955b071e6b099447e6aac3f2b318e82e
tacks9 <tacks321@qq.com>
2023-06-26 14:59:06 +0800

    🧪 test(code): 初步测试 gin 框架的 Http 服务
    curl http://127.0.0.1:54001/ping

---
+++ test.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// listen and serve on 0.0.0.0:54001
+	r.Run(":54001")
+}
