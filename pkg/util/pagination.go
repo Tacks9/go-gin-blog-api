c592b6a99d162698d3347b66086bf81e21fb8b8b
tacks9 <tacks321@qq.com>
2023-06-26 17:43:02 +0800

    ✨ feat(工具类管理): 封装分页相关方法 | util.GetPage

---
+++ pkg/util/pagination.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+
+	"go-gin-blog-api/pkg/setting"
+)
+
+// 获取对应分页的偏移量
+func GetPage(c *gin.Context) int {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
+
+	return result
+}
