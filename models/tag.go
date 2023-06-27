df93350c1a95395a88d381344abf247c7cd95fde
tacks9 <tacks321@qq.com>
2023-06-27 11:11:16 +0800

    ✨ feat(标签模块管理): 获取标签列表的逻辑实现，分页获取数据
    /api/v1/tags

---
+++ models/tag.go
@@ -0,0 +1,24 @@
+package models
+
+type Tag struct {
+	Model
+
+	Name       string `json:"name"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State      int    `json:"state"`
+}
+
+// 分页获取标签
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
+	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
+
+	return
+}
+
+// 获取标签总数
+func GetTagTotal(maps interface{}) (count int) {
+	db.Model(&Tag{}).Where(maps).Count(&count)
+
+	return
+}
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,11 +1,45 @@
 package v1
 
 import (
+	"go-gin-blog-api/models"
+	"go-gin-blog-api/pkg/e"
+	"go-gin-blog-api/pkg/setting"
+	"go-gin-blog-api/pkg/util"
+
 	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
 )
 
 // 获取多个文章标签
 func GetTags(c *gin.Context) {
+	// 接收 URL Query 参数
+	name := c.Query("name")
+
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+
+	// 参数检查
+	if name != "" {
+		maps["name"] = name
+	}
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+
+	// 分页获取数据
+	data["list"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+
+	okcode := e.SUCCESS
+
+	// 数据返回
+	c.JSON(okcode, gin.H{
+		"code":    okcode,
+		"message": e.GetMsg(okcode),
+		"data":    data,
+	})
 }
 
 // 新增文章标签
